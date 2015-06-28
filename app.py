19b34618e83553c3a559c92cb715db700118946b
Rohit Bhoopalam <rohitbr1@gmail.com>
2015-06-27 23:57:29 -0400

    Changed return type to suit Angular

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flaskext.mysql import MySQL
 import json
 from flask import jsonify
 from flask import request 
+from flask import Response
+from crossdomain import crossdomain
  
 mysql = MySQL()
 
@@ -18,60 +20,50 @@ mysql.init_app(app)
 import models as m
 
 @app.route('/questions')
+@crossdomain(origin='*')
 def get_questions():
     q_ref_id = request.args.get("q_ref_id", None)
-    return jsonify(data=m.get_top_questions(q_ref_id=q_ref_id))
+    return Response(json.dumps(m.get_top_questions(q_ref_id=q_ref_id)),  mimetype='application/json')
 
 @app.route('/insert_question', methods=["POST"])
+@crossdomain(origin='*')
 def insert_question():
     q_question = request.form.get("q_question", None)
     q_ref_id = request.form.get("q_ref_id", None)
     q_user_id = request.form.get("q_user_id", None)
 
-    if not (q_question and q_ref_id and q_user_id):
-        return jsonify(result=False, error_msg="q_question, q_ref_id, q_user_id cannot be empty")
+    m.insert_question(q_question, q_ref_id, q_user_id)
+    return Response(json.dumps(True),  mimetype='application/json')
 
-    if m.insert_question(q_question, q_ref_id, q_user_id):
-        return jsonify(result=True)
-    return jsonify(result=False, error_msg="Unknown error!!")
-
-@app.route('/upvote_question', methods=["POST"])
+@app.route('/upvote_question', methods=["GET", "POST"])
+@crossdomain(origin='*')
 def upvote_question():
-    q_id = request.form.get("q_id", None)
-    if not q_id:
-        return jsonify(result=False, error_msg="q_id cannot be empty")
-
-    if m.upvote_question(q_id):
-        return jsonify(result=True)
-    return jsonify(result=False, error_msg="Unknown error!!")
+    q_id = request.args.get("q_id", None)
+    return Response(json.dumps(True),  mimetype='application/json')
 
 @app.route('/answers')
+@crossdomain(origin='*')
 def get_answers():
     q_id = request.args.get("q_id", None)
-    return jsonify(data=m.get_top_answers(q_id))
+    return Response(json.dumps(m.get_top_answers(q_id)),  mimetype='application/json')
 
 @app.route('/insert_answer', methods=["POST"])
+@crossdomain(origin='*')
 def insert_answer():
     a_answer = request.form.get("a_answer", None)
     q_id = request.form.get("q_id", None)
     a_user_id = request.form.get("a_user_id", None)
 
-    if not (a_answer and q_id and a_user_id):
-        return jsonify(result=False, error_msg="a_answer, q_id, a_user_id cannot be empty")
-
-    if m.insert_answer(a_answer, q_id, a_user_id):
-        return jsonify(result=True)
-    return jsonify(result=False, error_msg="Unknown error!!")
+    m.insert_answer(a_answer, q_id, a_user_id)
+    return Response(json.dumps(True),  mimetype='application/json')
 
 @app.route('/upvote_answer', methods=["POST"])
+@crossdomain(origin='*')
 def upvote_answer():
     a_id = request.form.get("a_id", None)
-    if not a_id:
-        return jsonify(result=False, error_msg="a_id cannot be empty")
+    m.upvote_answer(a_id)
+    return Response(json.dumps(True),  mimetype='application/json')
 
-    if m.upvote_answer(a_id):
-        return jsonify(result=True)
-    return jsonify(result=False, error_msg="Unknown error!!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
